0bf2598e0474789a55d0d34fd2096fd455e19048
yoonza <zoony1110@sch.ac.kr>
2023-09-28 18:49:57 +0900

    update map data

---
+++ frontend/markers.js
@@ -0,0 +1,72 @@
+// 마커를 표시할 위치와 title 객체 배열입니다 
+var positions = [
+    {
+        title: '고고쓰 순천향대점', 
+        latlng: new kakao.maps.LatLng(36.7737209, 126.932562)
+    },
+    {
+        title: '카페 에이루트', 
+        latlng: new kakao.maps.LatLng(36.7734328, 126.932988)
+    },
+    {
+        title: '대단한탕후루 순천향대점카페', 
+        latlng: new kakao.maps.LatLng(36.7750554, 126.934062)
+    },
+    {
+        title: '베이커리경',
+        latlng: new kakao.maps.LatLng(36.7747488, 126.933670)
+    },
+    {
+        title: '카페별',
+        latlng: new kakao.maps.LatLng(36.7730993, 126.932786)
+    },
+    {
+        title: '파리바게뜨 아산순천향대점',
+        latlng: new kakao.maps.LatLng(36.7739109, 126.9338895)
+    },
+    {
+        title: '컴포즈커피 아산순천향대점',
+        latlng: new kakao.maps.LatLng(36.7737032, 126.932976)
+    },
+    {
+        title: '메가MGC커피 순천향대점',
+        latlng: new kakao.maps.LatLng(36.7735771, 126.933167)
+    },
+    {
+        title: '유캔두잇 아산순천향대점',
+        latlng: new kakao.maps.LatLng(36.7770113, 126.934945)
+    },
+    {
+        title: '빽다방 아산순천향대학교점',
+        latlng: new kakao.maps.LatLng(36.7733614, 126.934142)
+    },
+    {
+        title: '쥬씨 순천향대점',
+        latlng: new kakao.maps.LatLng(36.7731181, 126.934198)
+    },
+    {
+        title: '공차 아산순천향대점',
+        latlng: new kakao.maps.LatLng(36.7730993, 126.932786)
+    },
+    {
+        title: 'The대단한커피 순천향대점',
+        latlng: new kakao.maps.LatLng(36.7750554,126.934062)
+    },
+    {
+        title: '우주라이크커피 아산순천향대점',
+        latlng: new kakao.maps.LatLng(36.7770113, 126.934945)
+    },
+    {
+        title: '카페 15th Avenue',
+        latlng: new kakao.maps.LatLng(36.7741540, 126.933469)
+    },
+    {
+        title: '필나인',
+        latlng: new kakao.maps.LatLng(36.7699724,126.980594)
+    },
+];
+
+// 마커 이미지의 이미지 주소입니다
+var imageSrc = "/Users/yoonza/Desktop/Capstone/frontend/icons8-location-64.png"; 
+
+var imageSize = new kakao.maps.Size(30, 35); 
