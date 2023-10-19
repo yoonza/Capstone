1187d3c3a680711f66059a078dd8a74edbd1854c
YOONZA <68805229+yoonza@users.noreply.github.com>
2023-10-20 00:57:00 +0900

    change css

--- backend/realtime.php
+++ backend/realtime.php
@@ -1,20 +1,31 @@
 <?php
-	session_start();
+    session_start();
 
-	$conn = mysqli_connect("localhost", "root", "", "gande_member");
+    $conn = mysqli_connect("localhost", "root", "Forestz01!!", "gande_member");
 
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-	// 로그인 처리 후, 쿠키에 로그인 정보 저장
-	$is_login = false;
-	if ($is_login) {
-		$is_login = true;
-    	setcookie('username', $username, time() + 3600, '/');
-    	session_start();
-    	$_SESSION['username'] = $username;
-	}
+    $name = ''; // 빈 문자열로 $name을 초기화합니다.
+
+    // 사용자가 로그인한 경우 확인
+    if (isset($_SESSION['username']) || isset($_COOKIE['username'])) {
+        // 사용자가 로그인한 경우
+        if (isset($_SESSION['username'])) {
+            $username = $_SESSION['username'];
+            $sql = "SELECT name FROM members WHERE username = '{$username}'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result);
+            $name = $row['name']; // $name 업데이트
+        } else {
+            $username = $_COOKIE['username'];
+            $sql = "SELECT name FROM members WHERE username = '{$username}'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result);
+            $name = $row['name']; // $name 업데이트
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +35,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="realtime.css">
     <title>실시간 예약 페이지</title>
+    <script src="http://kit.fontawesome.com/e1a4d00b81.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="top">
@@ -32,38 +44,32 @@
         </a>
         <span class="right">
             <?php
-				if (isset($_SESSION['username']) || isset($_COOKIE['username'])) {
-				// 로그인 상태
-				if (isset($_SESSION['username'])) {
-    				$username = $_SESSION['username'];
-					$sql = "SELECT name FROM gande_member WHERE username = '{$username}'";
-        			$result = mysqli_query($conn, $sql);
-        			$row = mysqli_fetch_array($result);
-        			$name = $row['name'];
-					echo "<a href='mypage.php'><strong>{$name}님 환영합니다!</strong></a>";
-                    function redirect($url) {
-                        header('Location: contact.php'.$url);
-                        exit();
+                if (isset($_SESSION['username']) || isset($_COOKIE['username'])) {
+                    // 로그인 상태
+                    if (isset($_SESSION['username'])) {
+                        $username = $_SESSION['username'];
+                        $sql = "SELECT name FROM members WHERE username = '{$username}'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_array($result);
+                        $name = $row['name'];
+                        echo "<a href='mypage.php'><strong>{$name}님 환영합니다!</strong></a>";
+                    } else {
+                        $username = $_COOKIE['username'];
+                        $sql = "SELECT name FROM members WHERE username = '{$username}'";
+                        $result = mysqli_query($conn, $sql);
+                        $row = mysqli_fetch_array($result);
+                        $name = $row['name'];
+                        echo "<a href='mypage.php'><strong>{$name}님 환영합니다!</strong></a>";
                     }
-				} else {
-    				$username = $_COOKIE['username'];
-					$sql = "SELECT name FROM gande_member WHERE username = '{$username}'";
-        			$result = mysqli_query($conn, $sql);
-        			$row = mysqli_fetch_array($result);
-        			$name = $row['name'];
-        			echo "<a href='mypage.php'><strong>{$name}님 환영합니다!</strong></a>";
-				}
-				// 로그아웃 버튼 표시
-				echo '<a href="logout.php"><strong>로그아웃</strong></a>';
-
-				// echo '<li><a href="mypage.php">마이페이지</a></li>';
-				} else {
-				// 로그아웃 상태
-    			// 로그인 버튼 표시
-    			echo '<a href="login.php"><strong>로그인</strong></a>';
-				}
+                    // 로그아웃 버튼 표시
+                    echo '<a href="logout.php"><strong>로그아웃</strong></a>';
+                } else {
+                    // 로그아웃 상태
+                    // 로그인 버튼 표시
+                    echo '<a href="login.php"><strong>로그인</strong></a>';
+                }
                 echo '<a href="contact.php"><strong>고객센터</strong></a>';
-			?>
+            ?>
         </span>
     </div>
     <div class="reservation-info">
