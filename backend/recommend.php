e932aa4627ac5625931147736dd6037c78aa4fa1
YOONZA <68805229+yoonza@users.noreply.github.com>
2023-11-06 21:21:29 +0900

    Add files via upload

--- backend/recommend.php
+++ backend/recommend.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	$conn = mysqli_connect("localhost", "root", "", "gande_member");
+	$conn = mysqli_connect("localhost", "root", "Forestz01!!", "gande_member");
 
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
@@ -15,43 +15,6 @@
     	session_start();
     	$_SESSION['username'] = $username;
 	}
-
-    $cafe_name = "";
-    $address = "";
-    $latitude = "";
-    $longitude = "";
-    $member_name = "";
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // 폼에서 제출된 데이터 가져오기
-        $cafe_name = $_POST["cafe_name"];
-        $address = $_POST["address"];
-        $latitude = $_POST["latitude"];
-        $longitude = $_POST["longitude"];
-        $member_name = $_POST["member_name"];
-    
-        // SQL 쿼리 작성하여 데이터베이스에 카페 정보 삽입
-        $sql = "INSERT INTO cafes (cafe_name, address, latitude, longitude, member_name) VALUES (?, ?, ?, ?, ?)";
-        $stmt = mysqli_prepare($conn, $sql);
-    
-        // 바인딩 및 실행
-        mysqli_stmt_bind_param($stmt, "ssdss", $cafe_name, $address, $latitude, $longitude, $member_name);
-        $result = mysqli_stmt_execute($stmt);
-    
-        if ($result) {
-            echo '<script>alert("카페가 추가 되었습니다.");</script>';
-        } else {
-            echo '<script>alert("카페 등록 중 오류가 발생했습니다.");</script>';
-        }
-
-        // 새로운 카페 정보를 세션에 추가
-        if (!isset($_SESSION['cafes'])) {
-            $_SESSION['cafes'] = array();
-        }
-
-        // 카페 정보를 추가
-        $_SESSION['cafes'][] = array("name" => $cafe_name, "address" => $address);
-    }
 ?>
 
 <!DOCTYPE html>
@@ -65,9 +28,9 @@
         <meta name="keywords" content="css3, perspective, 3d, jquery, transform3d, responsive, template, restaurant, menu, leaflet, folded, flyer, concept" />
         <meta name="author" content="Codrops" />
         <!--<link rel="shortcut icon" href="../favicon.ico">--> 
-        <link rel="stylesheet" type="text/css" href="menu.css" />
-		<link href='http://fonts.googleapis.com/css?family=Raleway:300,500|Arvo:700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="recommend.css" />
 		<script type="text/javascript" src="custom.js"></script>
+		<script src="http://kit.fontawesome.com/e1a4d00b81.js" crossorigin="anonymous"></script>
 		<!--[if lte IE 8]><style>.support-note .note-ie{display:block;}</style><![endif]-->
     </head>
     <body>
@@ -84,22 +47,18 @@
     				// 로그인 상태
     				if (isset($_SESSION['username'])) {
         				$username = $_SESSION['username'];
-						$sql = "SELECT name FROM gande_member WHERE username = '{$username}'";
+						$sql = "SELECT name FROM members WHERE username = '{$username}'";
             			$result = mysqli_query($conn, $sql);
             			$row = mysqli_fetch_array($result);
             			$name = $row['name'];
 						echo "<a href='mypage.php'><strong>{$name}님 환영합니다!</strong></a>";
-
-                        // 'roles' 값을 설정합니다.
-                        $_SESSION['roles'] = 'owner';
-                        $_SESSION['roles'] = 'customer';
                         function redirect($url) {
-                            header('Location: menu.php'.$url);
+                            header('Location: recommend.php'.$url);
                             exit();
                         }
     				} else {
         				$username = $_COOKIE['username'];
-						$sql = "SELECT name FROM gande_member WHERE username = '{$username}'";
+						$sql = "SELECT name FROM members WHERE username = '{$username}'";
             			$result = mysqli_query($conn, $sql);
             			$row = mysqli_fetch_array($result);
             			$name = $row['name'];
@@ -122,11 +81,8 @@
             </div><!--/top bar -->
 			
 			<header>
-				<h1>Welcome to GANDE's MENU</h1>
-				<h2>
-                    여러분의 취향을 담아 드립니다. <a href="main.php">GANDE</a>
-				</h2>
-				
+				<h1>Welcome to GANDE's MENU</h1><br>
+
 				<!--<div class="support-note">--><!-- let's check browser support with modernizr -->
 				    <!--<span class="no-cssanimations">CSS animations are not supported in your browser</span>
 					<span class="no-csstransforms">CSS transforms are not supported in your browser</span>
@@ -162,9 +118,10 @@
 									<div class="rm-info">
 										<p>
 										<br>
-										<strong>Copyright 2023. Gande ALL RIGHTS RESERVED.</strong><br>
-										<strong>EMAIL</strong> gracekim0513@naver.com<br>
-										<strong>EMAIL</strong> wldbs1110@naver.com</p>
+										<strong>Copyright 2023<br> 
+											Gande ALL RIGHTS RESERVED.</strong><br>
+										EMAIL) gracekim0513@naver.com<br>
+										EMAIL) wldbs1110@naver.com</p>
 									</div>
 
 								</div><!-- /rm-content -->
@@ -177,7 +134,7 @@
 								if (isset($_SESSION['username'])) {
     								// 설정된 경우, 사용자 이름을 가져옵니다.
     								$username = $_SESSION['username'];
-    								$sql = "SELECT name FROM gande_member WHERE username = '{$username}'";
+    								$sql = "SELECT name FROM members WHERE username = '{$username}'";
     								$result = mysqli_query($conn, $sql);
 
     								// SQL 쿼리가 정상적으로 실행되었는지 확인합니다.
@@ -189,59 +146,36 @@
     								$row = mysqli_fetch_array($result);
     								if ($row) {
         								$name = $row['name'];
-        								if (isset($_SESSION['roles']) && $_SESSION['roles'] === 'owner') {
-                                            echo "<h4>{$name} 사장님의 카페 등록</h4>";
-                                        } else {
-                                            echo "<h4>{$name}님의 선호 카페</h4>";
-                                        }
+        								echo "<h4>{$name}님의 선호 카페</h4>";
     								} else {
         								echo "<p>로그인 후 선호하는 카페를 등록해주세요.</p>";
     								}
 								} else {
     								// 설정되지 않은 경우, 로그인 후 선호하는 카페를 등록하라는 메시지를 출력합니다.
-    								echo "<p>로그인 후 선호하는 카페를 등록해주세요.</p>";
+    								echo "<p>로그인 후 선호하는 카페를 등록하세요!.</p>";
 								}
 								?>
 									<dl>
+										<dt>1. 카페 리스트 추천 기능</dt>
+										<dd>매일 간디 그룹에서 직접 추천 카페 리스트를 제공합니다.</dd>
 
-                                        <div class="rm-order">
-										    <?php
-										        if (isset($_SESSION['roles']) && $_SESSION['roles'] === 'owner') {
-                                                    echo '<p><strong>카페 사장님이시라면?</strong><br>카페 등록 하기를 통해 사장님의 카페를 알려주세요!<br>';
-											        echo '<a href = "new_cafe.php"><button type = "button">카페 등록하기</button></a>';
-                                                    echo '<dt>만약 카페 사장님이 아닌 경우 카페를 등록하셨다면, 관리자에 의해 카페 등록이 취소 될 수 있습니다.<dt><br>';
-										        } else {
-											        echo '<dt><a href="cafelist.php">선호하는 카페 추가하기</a><dt><br>';
-										        }
-										    ?>
-										    </p>
-									    </div>
-                                        <?php
-                                        // cafes 테이블에서 멤버 이름이 gande_member 테이블의 name과 같은 행을 가져온다.
-                                        $sql = "SELECT * FROM cafes WHERE member_name = '{$name}'";
-                                        $result = mysqli_query($conn, $sql);
+										<dt>2. 사용자의 위치 인근의 카페 검색 기능</dt>
+										<dd>고객이 직접 인근의 카페를 찾고 방문할 수 있습니다.</dd>
 
-                                        $count = 5;
-                                        $index = 1;
-                                        while ($row = mysqli_fetch_assoc($result)) {
-                                            if ($index > $count) {
-                                                break;
-                                            }
-                                            $cafe_name = $row['cafe_name'];
-                                            $address = $row['address'];
-                                            echo "<dt>{$index}. {$cafe_name}</dt>";
-                                            echo "<dd>{$address}</dd>";
-                                            $index++;
-                                        }
-                                        ?>
+										<dt>3. 회원형 맞춤 서비스 제공</dt>
+										<dd>회원/비회원에 따라 선택형 서비스를 제공합니다.</dd>
+
+										<dt>4. 서비스 개선 상시 반영</dt>
+										<dd>불편사항 혹은 개선사항은 직접 문의가 가능합니다.</dd>
+										
+										<dt>5. 최상의 서비스 제공, 최대의 카페 정보 제공</dt>
+										<dd>항시 무료 프로그램을 제공하며, 원하는 카페를 모두 검색할 수 있습니다.</dd>
 									</dl>
 
 									<h4>카페 예약 하러 가기</h4>
 									<dl>
-										<dt><a href="reservation.php">예약하러 가기</a></dt><br><br>
-
-										<dt>카페를 선택해서 좌석 예약 후, 날짜와 시간을 선택하세요 !</dt>
-										<dd>GANDE를 통해 즐겁고 행복한 하루 되세요</dd>
+										<dt>예약페이지로 이동하세요!</dt>
+										<dd><a href="reservation.php" style="font-weight: bold; color: red;">예약사이트 이동</a></dd>
 									</dl>
 								</div><!-- /rm-content -->
 								<div class="rm-overlay"></div>
@@ -253,40 +187,24 @@
 						<div class="rm-middle">
 							<div class="rm-inner">
 								<div class="rm-content">
-									<h4>위치 별 카페 추천</h4>
+									<h4>위치별 카페 추천</h4>
 									<dl>
-										<dt><a href="seoul_search.php">서울 카페 추천</a></dt>
-										<dd>서울에 있는 카페를 검색해주세요</dd>
+										<dt><a href="http://localhost/cafelist.php" style="font-weight:bold; text-decoration: none; color: red;">카페 검색하기</a></dt>
+										<dd><strong>서울 / 경기 / 대전 / 세종 / 강원 / 광주 / 대구 / 울산 / 부산 / 경북 / 경남 / 전남 / 전북 / 제주 / 충북 / 충남 / 인천</strong></dd>
 
-										<dt><a href="gyeonggi_search.php">경기도 카페 추천</a></dt>
-										<dd>경기도에 있는 카페를 검색해주세요</dd>
-
-										<dt><a href="chungbuk_search.php">충청북도 카페 추천</a></dt>
-										<dd>충북에 있는 카페를 검색해주세요</dd>
-
-										<dt><a href="chungnam_search.php">충청남도 카페 추천</a></dt>
-										<dd>충남에 있는 카페를 검색해주세요</dd>
+										<dt><a href="http://localhost/faq.php" style="font-weight:bold; color: red;">자주 묻는 질문(FAQ)</a></dt>
+										<dd><strong>GANDE에 관련된 궁금한 사항은 FAQ에서 확인할 수 있습니다.</strong></dd>
+										<dt>Maple Barbeque Tofu</dt>
+										<dd>Grilled marinated tofu, maple barbeque sauce, tahini slaw, grilled seasonal vegetables and mashed potatoes</dd>
+																													
+										<dt><a href="http://herbivoracious.com/2012/07/king-oyster-mushroom-with-roasted-cherries-and-sage-no-that-isnt-meat-recipe-and-thought-process.html" class="rm-viewdetails" data-thumb="images/4.jpg">Luxur Oyster</a></dt>
+										<dd>King oyster mushroom with roasted cherries and sage</dd>
 										
-										<dt><a href="jeonbuk_search.php">전라북도 카페 추천</a></dt>
-										<dd>전북에 있는 카페를 검색해주세요</dd>
+										<dt><a href="http://herbivoracious.com/2012/09/rigatoni-with-roasted-cauliflower-and-spicy-tomato-sauce-recipe.html" class="rm-viewdetails" data-thumb="images/3.jpg">Rigatoni di Cavolfiore</a></dt>
+										<dd>Rigatoni with roasted cauliflower and spicy tomato sauce</dd>
 										
-										<dt><a href="jeonnam_search.php">전라남도 카페 추천</a></dt>
-										<dd>전남에 있는 카페를 검색해주세요</dd>
-
-                                        <dt><a href="gyeongbuk_search.php">경상북도 카페 추천</a></dt>
-										<dd>경북에 있는 카페를 검색해주세요</dd>
-
-                                        <dt><a href="gyeongnam_search.php">경상남도 카페 추천</a></dt>
-										<dd>경남에 있는 카페를 검색해주세요</dd>
-
-                                        <dt><a href="gangwon_search.php">강원도 카페 추천</a></dt>
-										<dd>강원에 있는 카페를 검색해주세요</dd>
-
-                                        <dt><a href="jeju_search.php">제주도 카페 추천</a></dt>
-										<dd>제주에 있는 카페를 검색해주세요</dd>
-
-                                        <dt><a href="광역시_search.php">광역시 카페 추천</a></dt>
-										<dd>인천, 세종, 대전, 울산, 광주에 있는 카페를 검색해주세요</dd>
+										<dt><a href="http://herbivoracious.com/2012/06/saffron-chickpea-stew-with-grilled-porcini-mushroom-recipe.html" class="rm-viewdetails" data-thumb="images/14.jpg">Saffron Chana Secret</a></dt>
+										<dd>Saffron chickpea stew with grilled porcini mushrooms</dd>
 									</dl>
 								</div><!-- /rm-content -->
 								<div class="rm-overlay"></div>
@@ -302,16 +220,16 @@
 							<div class="rm-back">
 								<span class="rm-close">Close</span>
 								<div class="rm-content">
-									<h4>Gande's Hot Place</h4>
+								<h4>오늘의 추천 카페리스트</h4>
                                     <!--알고리즘 추천 -->
 									<dl>
-										<dt><a href="https://www.starbucks.co.kr/index.do">스타벅스 강남R점</a></dt>
-										<dd>시그니처 로스팅, 가벼운 식사, 무료 Wi-Fi로 잘 알려진 미국 시애틀 기반의 커피하우스 체인점입니다!</dd>
+										<dt><a href="https://www.starbucks.co.kr/index.do">스타벅스</a></dt>
+										<dd>시그니처 로스팅, 가벼운 식사, 무료 Wi-Fi로 잘 알려진 미국 시애틀 기반의 커피하우스 전국 체인점입니다!</dd>
 										
 										<dt><a href="https://www.instagram.com/cafe_annac">카페 안낙</a></dt>
 										<dd>아산 신정호에 위치한 카페. 맛있는 커피와 디저트와 함께 아름다운 호수뷰를 구경하세요!</dd>
 										
-										<dt><a href="https://www.terarosa.com/market/main?viewName=redirect%3A%2Fmarket%2Fmain">테라로사 커피공장 강릉본점</a></dt>
+										<dt><a href="https://www.terarosa.com/market/main?viewName=redirect%3A%2Fmarket%2Fmain">테라로사 커피공장</a></dt>
 										<dd>아름다운 강릉에서 커피 로스터리로 시작, 한국의 스페셜티 커피의 선구자인 테라로사에 방문하세요!</dd>
 										
 										<dt><a href="https://www.instagram.com/flower_illda/">카페 일다</a></dt>
@@ -320,9 +238,13 @@
 										<dt><a href="https://www.instagram.com/cafe_toenmaru/">카페 툇마루</a></dt>
 										<dd>흑임자커피와 두부케이크가 유명한 카페 툇마루입니다. 초심을 잃지 않고 한잔한잔 준비합니다!</dd>
 										
-										<dt><a href="https://www.instagram.com/allthatcoffee/">올댓커피 본점</a></dt>
-										<dd>에스프레소 및 커피와 디저트를 판매하는 한국의 에스프레소바 카페입니다.</dd>
 									</dl>
+									<br>
+									<div class="rm-order">
+										<p><strong>카페 사장님이시라면?</strong><br>카페 등록 하기를 통해 사장님의 카페를 알려주세요!<br>
+										<a href = "new_cafe.php"><button type = "button" style="border-radius:5px; border:2px solid #ddd; color: black;">카페 등록하기</button></a>
+										</p>
+									</div>
 								</div><!-- /rm-content -->
 							</div><!-- /rm-back -->
 
@@ -336,7 +258,7 @@
         </div>
 		<!-- jQuery if needed -->
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-        <script type="text/javascript" src="menu.js"></script>
+        <script type="text/javascript" src="recommend.js"></script>
 		<script type="text/javascript">
 			$(function() {
 
